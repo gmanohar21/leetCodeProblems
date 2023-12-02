2fb80541efe68a54fb8d94413e525f4669ad4955
Manu <66511367+gmanohar21@users.noreply.github.com>
2023-12-02 13:25:30 +0530

    Time: 15 ms (48.15%) | Memory: 44.1 MB (56.30%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -1,27 +1,44 @@
 class Solution {
-	public static int countCharacters(String[] words, String chars) {
-		int ans = 0;
-		for (int i = 0; i < words.length; i++) {
-			String word = words[i];
-			char charArr[] = word.toCharArray();
-			char mainArr[] = chars.toCharArray();
-			 char charArr1[]=mainArr;
-			int charMatch = 0;
-			for (int j = 0; j < charArr.length; j++) {
-				for (int k = 0; k < charArr1.length; k++) {
-					if(charArr1[k]!='&')
-					if (charArr[j] == charArr1[k]) {
-						charMatch++;
-						charArr1[k]='&';
-						break;
-					}
-				}
-			}
-			if (charMatch == word.length()) {
-				int t = word.length();
-				ans += t;
-			}
-		}
-		return ans;
-	}
+    public static int countCharacters(String[] words, String chars) {
+        // Initialize a variable to store the sum of lengths
+        int ans = 0;
+        
+        // Iterate through each word in the array
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            char[] charArr = word.toCharArray();
+            char[] mainArr = chars.toCharArray();
+            
+            // Iterate through each character in the word
+            boolean canFormWord = true;
+            for (int j = 0; j < charArr.length; j++) {
+                boolean charFound = false;
+                
+                // Check if the character exists in chars and hasn't been used before
+                for (int k = 0; k < mainArr.length; k++) {
+                    if (mainArr[k] != '&') {
+                        if (charArr[j] == mainArr[k]) {
+                            charFound = true;
+                            mainArr[k] = '&';  // Mark the character as used
+                            break;
+                        }
+                    }
+                }
+                
+                // If character not found or already used, break the loop
+                if (!charFound) {
+                    canFormWord = false;
+                    break;
+                }
+            }
+            
+            // If the word can be formed, update the sum
+            if (canFormWord) {
+                ans += word.length();
+            }
+        }
+        
+        // Return the final sum
+        return ans;
+    }
 }
