9b0deace64acafae4556386b4692791911fb658c
Manu <66511367+gmanohar21@users.noreply.github.com>
2023-12-05 08:45:08 +0530

    Time: 0 ms (100.00%) | Memory: 39.2 MB (49.20%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -1,5 +1,26 @@
 class Solution {
     public int numberOfMatches(int n) {
-        return n-1;
+       //only one winner will come at the end 
+       //TC=O(1)
+       return n-1;
     }
 }
+
+
+//first approach simulation
+/* 
+tc=O(logn)  we can improve TC by O(1)
+  public int numberOfMatches(int n) {
+        int ans=0;
+        while(n>1){
+            if(n%2==0){
+                ans+=n/2;
+                n=n/2;
+            }else{
+                 ans+=(n-1)/2;
+                n=(n-1)/2+1;
+            }
+        }
+        return ans;
+    }
+*/
