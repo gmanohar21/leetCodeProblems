5a30e22dfbb910ff1d51530c2923f45792a46616
Manu <66511367+gmanohar21@users.noreply.github.com>
2024-01-21 01:16:56 +0530

    Time: 14 ms (15.37%) | Memory: 45.3 MB (55.66%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -7,12 +7,9 @@ class Solution {
         }
         for(Map.Entry<Integer,Integer> s:m.entrySet()){
            
-                if(s.getValue()==1)  {
+                if(s.getValue()==1)
                     ans=s.getKey();
-                    break;
-                }
-           
-            
+                 
         }
         return ans;
     }
