fb0ddf4de951486053279ee38f69a432530e26d9
Manu <66511367+gmanohar21@users.noreply.github.com>
2024-02-22 08:38:43 +0530

    Time: 1 ms (85.96%) | Memory: 42.6 MB (95.25%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,15 +1,16 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans=new ArrayList<>();
-        bt(ans,"",0,0,n);
+        genFn(ans,0,0,n,"");
         return ans;
     }
-   public void bt(List<String> l,String s,int open,int close,int max){
-       if(s.length()==max*2){
-           l.add(s);
-           return;
-       }
-       if(open<max) bt(l,s+"(",open+1,close,max);
-       if(close<open) bt(l,s+")",open,close+1,max);
+    public void genFn( List<String> ans,int open,int close,int n,String data){
+        if(data.length()==n*2){
+            ans.add(data);
+            return;
+        }
+         
+       if(open<n) genFn(ans,open+1,close,n,data+"(");
+        if(close < open)genFn(ans,open,close+1,n,data+")");
     }
 }
