486c8ae447ab18de6e75cc99bd3403c2cc7c2eda
Manu <66511367+gmanohar21@users.noreply.github.com>
2024-03-14 02:03:42 +0530

    Time: 1 ms (77.68%) | Memory: 41.3 MB (11.68%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,12 +1,15 @@
 class Solution {
-    public int pivotInteger(int n) {
-        int totalSum = n * (n + 1) / 2;
-        int pivot = (int) Math.sqrt(totalSum);
-
-        if (pivot * pivot == totalSum) {
-            return pivot;
-        } else {
-            return -1;
+     public int pivotInteger(int n) {
+        int[] prefix = new int[n+1];
+        prefix[0] = 0;
+        for(int i = 1;i<=n;i++){
+            prefix[i] = prefix[i-1] + i;
         }
+        for(int i = 1;i<=n;i++){
+            if(prefix[i] == prefix[n] - prefix[i-1]){
+                return i;
+            }
+        }
+        return -1;
     }
 }
